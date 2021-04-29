929f077c234768d87cc2f539bb5dc1c558a46b92
peanut-lover <jihoson94@gmail.com>
2021-04-29 17:08:52 +0900

    Optimize Order DTO Query

--- src/main/java/jpabook/jpashop/repository/order/query/OrderItemQueryDto.java
+++ src/main/java/jpabook/jpashop/repository/order/query/OrderItemQueryDto.java
@@ -4,13 +4,13 @@ import lombok.Data;
 
 @Data
 public class OrderItemQueryDto {
-    private Long id;
+    private Long orderId;
     private String name;
     private int orderPrice;
     private int count;
 
     public OrderItemQueryDto(Long id, String name, int orderPrice, int count) {
-        this.id = id;
+        this.orderId = id;
         this.name = name;
         this.orderPrice = orderPrice;
         this.count = count;
